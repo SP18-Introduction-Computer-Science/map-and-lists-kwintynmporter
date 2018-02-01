b08ed0810db66fb772eac8920e8a2eae0ac8ded1
Kwintyn Porter <kwintynmporter@lewisu.edu>
2018-02-01 16:23:56 -0600

    Maps and Lists Assignment

---
+++ mapsandlists1.py
@@ -0,0 +1,16 @@
+myList = [ 10, 20, 30, 40, 50 ] 
+print(myList) 
+
+listpointer = [ 0, 1, 2, 3, 4]
+
+myMap = { 0: " My ", 1: " Name ", 2 : " Is ", 3 : " Kwintyn ", 4 : " Porter " }
+
+
+print(myMap) 
+
+for loopVar in myMap :
+    print(loopVar) 
+
+for loopVar in myMap :
+    print(myMap[loopVar])
+
